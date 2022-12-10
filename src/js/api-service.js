a145a84cf98c1e3f18df487c3d5503ce3a3f2ea1
max-ignat <cadillac.maxim@gmail.com>
2022-12-10 13:51:53 +0100

    summary

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -9,14 +9,15 @@ export default class ApiService {
     const BASE_URL =
       'https://pixabay.com/api/?key=31187962-e7df80d652d1f0f281ee6ae38';
     return fetch(
-      `${BASE_URL}&q=${this.searchQuery}&&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=5`
+      `${BASE_URL}&q=${this.searchQuery}&&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=30`
     )
       .then(response => response.json())
       .then(data => {
         this.page += 1;
 
         return data.hits;
-      });
+      })
+      // .catch(error => (console.log(error)));
   }
 
   resetPage() {
--- src/js/render.js
+++ src/js/render.js
@@ -2,7 +2,7 @@ import { getItemsTemplate } from "./cardTemplate";
 
 const gallery = document.querySelector('.gallery');
 
-export function render(items) {
-  const markup = items.map(getItemsTemplate).join('');
+export function render(hits) {
+  const markup = hits.map(getItemsTemplate).join('');
   return gallery.insertAdjacentHTML('beforeend', markup);
 }
