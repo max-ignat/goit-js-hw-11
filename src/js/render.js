340de3ec1cc2201a386e8a2f7163f7b06c092882
max-ignat <cadillac.maxim@gmail.com>
2022-12-11 14:33:09 +0100

    update

--- src/js/render.js
+++ src/js/render.js
@@ -2,5 +2,5 @@ import { cardTemplate } from './cardTemplate';
 
 export function render(hits) {
   const markup = hits.map(cardTemplate).join('');
-  return refs.gallery.insertAdjacentHTML('beforeend', markup);
+  return gallery.insertAdjacentHTML('beforeend', markup);
 }
