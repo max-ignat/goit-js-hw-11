618e7d888f7a662a28c53463f00ee27720774996
max-ignat <cadillac.maxim@gmail.com>
2022-12-13 09:17:26 +0100

    without a/a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import * as ApiService from './js/api-service';
+import ApiService from './js/api-service';
 import Notiflix from 'notiflix';
-import { cardTemplate } from './js/cardTemplate';
+// import { cardTemplate } from './js/cardTemplate';
 import { render } from './js/render';
 import SimpleLightbox from 'simplelightbox';
 import './sass/index.scss';
@@ -12,53 +12,51 @@ const lightbox = new SimpleLightbox('.gallery a', {
   scrollZoom: false,
 });
 
- const refs = {
+const refs = {
   inputEl: document.querySelector('#search-form'),
 
   submitBtn: document.querySelector('button'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
+const perPage = 40;
+// class ApiService {
 
-class ApiService {
-  
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
+//   constructor() {
+//     this.searchQuery = '';
+//     this.page = 1;
+//   }
 
-  fetchImages() {
-    console.log('before', this);
-    const BASE_URL =
-      'https://pixabay.com/api/?key=31187962-e7df80d652d1f0f281ee6ae38';
-    return fetch(
-      `${BASE_URL}&q=${this.searchQuery}&&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=30`
-    )
-      .then(response => response.json())
-      .then(data => {
-        this.page += 1;
+//   fetchImages() {
+//     console.log('before', this);
+//     const BASE_URL =
+//       'https://pixabay.com/api/?key=31187962-e7df80d652d1f0f281ee6ae38';
+//     return fetch(
+//       `${BASE_URL}&q=${this.searchQuery}&&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=30`
+//     )
+//       .then(response => response.json())
+//       .then(data => {
+//         this.page += 1;
 
-        return data.hits;
-      })
-      .catch(error => console.log(error));
-  }
+//         return data.hits;
+//       })
+//       .catch(error => console.log(error));
+//   }
 
-  resetPage() {
-    this.page = 1;
-  }
-  get query() {
-    return this.searchQuery;
-  }
+//   resetPage() {
+//     this.page = 1;
+//   }
+//   get query() {
+//     return this.searchQuery;
+//   }
 
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-}
-
-
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+// }
 
 const apiService = new ApiService();
-
+console.log(apiService.page);
 refs.inputEl.addEventListener('submit', onSubmitClick);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
@@ -73,11 +71,14 @@ function onSubmitClick(event) {
   apiService.resetPage();
 
   apiService.fetchImages().then(data => {
-    if (data.length === 0) {
+    if (data.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
+    }
+    if (data.totalHits === 0) {
+      ifError();
     }
     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
     render(data);
@@ -93,15 +94,30 @@ function onLoadMore() {
       );
       return;
     }
-    Notiflix.Notify.success(`Hooray! We found more ${data.totalHits} images.`);
-    render(data);
+ render(data);
     lightbox.refresh();
+    const totalPages = Math.ceil(data.totalHits / perPage);
+
+    if (apiService.page > totalPages) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      ifError();
+      return;
+    }
+
+   
   });
 }
-// function render(data) {
-//   refs.gallery.insertAdjacentHTML('beforeend', cardTemplate(data));
-// }
 
 function clearRender() {
   refs.gallery.innerHTML = '';
+}
+
+function ifError() {
+  Notiflix.Notify.failure(
+    "Sorry, but you've reached the end of search results"
+  );
+}
+
+function hideButton() {
+  refs.loadMoreBtn.classList.add('is-hidden');
 }
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,33 +1,35 @@
-// export class ApiService {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
+const axios = require('axios');
+import Notiflix from 'notiflix';
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
 
-//   fetchImages() {
-//     console.log('before', this);
-//     const BASE_URL =
-//       'https://pixabay.com/api/?key=31187962-e7df80d652d1f0f281ee6ae38';
-//     return fetch(
-//       `${BASE_URL}&q=${this.searchQuery}&&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=30`
-//     )
-//       .then(response => response.json())
-//       .then(data => {
-//         this.page += 1;
+  fetchImages() {
+    // console.log('before', this);
+    const BASE_URL =
+      'https://pixabay.com/api/?key=31187962-e7df80d652d1f0f281ee6ae38';
+    return fetch(
+      `${BASE_URL}&q=${this.searchQuery}&&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.page += 1;
+        // console.log('data.totalHits', data.totalHits)
+        return data;
+      })
+      .catch(error => Notiflix.Notify.failure('Ups something wrong'));
+  }
 
-//         return data.hits;
-//       })
-//       .catch(error => (console.log(error)));
-//   }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
 
-//   resetPage() {
-//     this.page = 1;
-//   }
-//   get query() {
-//     return this.searchQuery;
-//   }
-
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-// }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
